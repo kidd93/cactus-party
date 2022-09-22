e730469a9e213204f21eef3deb080a14b43702f9
kelsie kidd <kelsiekidd93@gmail.com>
2022-09-22 13:31:54 -0500

    completed models'

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,31 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tags, {
+  through: ProductTag,
+  as: 'joined_product',
+  foreignKey: 'product_id'
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tags.belongsToMany(Products, {
+  through: ProductTag,
+  as: 'joined_product',
+  foreignKey: 'tags_id'
+});
 
 module.exports = {
   Product,
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,17 +4,42 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name'],
+      }
+    ]
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.param.id
+    },
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name'],
+      }
+    ]
+  })
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+
+  })
 });
 
 router.put('/:id', (req, res) => {
